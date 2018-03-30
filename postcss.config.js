84f72462f48e04e55fdb303e5ecba449c554d93d
tokimari <mari7toki@gmail.com>
2018-03-30 17:41:36 +0900

    chore: add ameba-color-pallete.css & watch style

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: [
+    require('postcss-custom-properties')(),
     require('postcss-import')(),
     require('autoprefixer')(),
     require('postcss-single-charset')(),
