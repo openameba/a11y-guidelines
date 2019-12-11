3c37822b85ce95077d970b9e4213ffabfcc2b917
sasaplus1 <sasaplus1@gmail.com>
2019-12-11 22:23:36 +0900

    postcss configs move to package.json

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [
-    require('postcss-custom-properties')(),
-    require('postcss-import')(),
-    require('autoprefixer')(),
-    require('postcss-single-charset')(),
-  ]
-};
