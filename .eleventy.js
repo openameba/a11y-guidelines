b60771e9be24d3c9cb6fd14981613cc894ba4c23
masuP9 <masuP9@gmail.com>
2021-01-03 14:11:47 +0900

    chore: setting markdown

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,20 @@
 module.exports = (eleventyConfig) => {
+  // Build setting
   eleventyConfig.addPassthroughCopy("src/img");
   eleventyConfig.addPassthroughCopy("src/favicon.ico");
   eleventyConfig.addPassthroughCopy("src/.nojekyll");
 
+  // Render markdown settings
+  const markdownIt = require("markdown-it");
+  const markdownItAnchor = require("markdown-it-anchor");
+  const markdownLib = markdownIt({
+    html: true,
+    linkify: false,
+  }).use(markdownItAnchor, { permalink: false });
+
+  eleventyConfig.setLibrary("md", markdownLib);
+
+  // Serve setting
   eleventyConfig.addWatchTarget("./src/styles");
 
   eleventyConfig.setBrowserSyncConfig({
@@ -11,17 +23,20 @@ module.exports = (eleventyConfig) => {
     startPath: "/a11y-guidelines",
   });
 
+  // Filters
   const filterByGuidelineNumber = require("./src/_filters/filterByGuidelineNumber");
   eleventyConfig.addFilter("filterByGuidelineNumber", filterByGuidelineNumber);
 
   const scNumberToPath = require("./src/_filters/scNumberToPath");
   eleventyConfig.addFilter("scNumberToPath", scNumberToPath);
 
+  // ShortCodes
   const labelShortCode = require("./src/_shortcodes/label");
   eleventyConfig.addPairedShortcode("label", labelShortCode);
 
   const scToNumberForCompare = require("./src/_utils/scToNumberForCompare");
 
+  // Collections
   eleventyConfig.addCollection("SC", (collectionApi) => {
     const SC = collectionApi.getFilteredByTag("sc").sort((a, b) => {
       return (
